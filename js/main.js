ba37d9dd6b5afcb6e467ab0ad9c6a707f604b9fc
Adam262 <abarcan@gmail.com>
2014-11-01 18:30:04 -0400

    responsive works, except for stupid sizing bug

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
-
+ 
     $('#fullpage').fullpage();
     console.log("Got this far");
+
+    $('#hamburger').on("click", function(){
+       $('#responsive').toggleClass("not-collapsed", 1000);
+    })
 });
